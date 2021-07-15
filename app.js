651b8faac94cdd4e9b3ed33455c0ebd766e488e4
carring10 <hartis.c99@gmail.com>
2021-07-14 20:48:43 -0400

    first commit, added html, js and css files

---
+++ app.js
@@ -0,0 +1,26 @@
+const riddles = [
+    {
+        question : "riddle 0", 
+        answer : "0"
+    },
+    {
+        question : "riddle 1", 
+        answer : "1"
+    },
+    {
+        question : "riddle 2", 
+        answer : "2"
+    },
+    {
+        question : "riddle 3", 
+        answer : "3"
+    },
+    {
+        question : "riddle 4", 
+        answer : "4"
+    },
+]
+
+function handleButtonClick () {
+    alert("something");
+}
